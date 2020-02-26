dfab2a3fb357457343999d5c7cf07cc121cde2b0
Raúl Martín <raul.martin@urjc.es>
2020-02-26 10:55:35 +0100

    Fix bad merge

--- client-demo/src/main/java/rmartin/lti/demo_plugin/DemoMain.java
+++ client-demo/src/main/java/rmartin/lti/demo_plugin/DemoMain.java
@@ -16,11 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 // Scan all entities and components that are inside any of the following packages
 @ComponentScan(basePackages = {"rmartin.lti"})
-<<<<<<< HEAD
 @EntityScan(basePackages = {"rmartin.lti.demo_plugin"})
-=======
-@EntityScan(basePackages = {"rmartin.lti"})
->>>>>>> 02d4d794a97fb77bb2b9226c82ff88566de91da7
 public class DemoMain {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMain.class, args);
